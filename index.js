b7bd6f9d111f5d9644ef40ab9a5e8dad127cffac
Shchepkin Alex <shchepkin.alex@gmail.com>
2017-04-08 12:19:33 +0300

    commit all

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 
 let bubbleSort = require('./bubble-sort');
 let coverByTile = require('./entry-test/coverByTile');
+let UnionFind = require('./unionFind');
+let bitPlusPlus = require('./bitPlusPlus');
 
 let algolib = {
     bubbleSort,
-    coverByTile
+    coverByTile,
+    UnionFind,
+    bitPlusPlus
+
 }
 
 module.exports = algolib;
