f92b3bb83c06d38119897be46fc5c38df1b403fb
Shchepkin Alex <shchepkin.alex@gmail.com>
2017-04-23 18:09:19 +0300

    linkedList with tests

--- linkedList.js
+++ linkedList.js
@@ -21,13 +21,17 @@ class LinkedList {
            obj.next =  this.last;
            this._elements[this.last - 1] = obj;
         }
-
         return this._elements;
     }
 
     getNextLink(a){
         let obj = this._elements[a];
         return obj.next;
+    }
+
+    getPrewLink(a){
+        let obj = this._elements[a];
+        return obj.prew;
     }
 
     get(a){
@@ -41,22 +45,60 @@ class LinkedList {
         }else return this.last;
     }
 
+
+    checkIsLast(a){
+        if ("next" in a){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
     del(a){
         if (a > this.last || a < 0){return NaN}
-        let objOld = this._elements[a];
 
-        delete this._elements[a];
+        let objForDelete = this._elements[a];
 
         if (this.last === 0) {
             this.last = -1;
 
-        }else if (a !== 0){
-           let obj = this._elements[a - 1];
-           obj.next =  objOld.next;
-           this._elements[a - 1] = obj;
-        }
-    }
+        }else {
+            if (this.checkIsLast(objForDelete)) {
+                if ("prew" in objForDelete) {
+                    let obj = this._elements[objForDelete.prew];
+                    delete obj.next;
+                    this._elements[objForDelete.prew] = obj;
+                    this.last = objForDelete.prew;
+                } else {
+                    let obj = this._elements[a - 1];
+                    delete obj.next;
+                    this._elements[a - 1] = obj;
+                    this.last = a - 1;
+                }
+            } else {
+                if ("prew" in objForDelete) {
+                    let obj = this._elements[objForDelete.prew];
+                    obj.next = objForDelete.next;
+                    this._elements[objForDelete.prew] = obj;
 
+                    obj = this._elements[objForDelete.next];
+                    obj.prew = objForDelete.prew;
+                    this._elements[objForDelete.next] = obj;
+
+                } else {
+                    let obj = this._elements[a - 1];
+                    obj.next = objForDelete.next;
+                    this._elements[a - 1] = obj;
+
+                    obj = this._elements[a + 1];
+                    obj.prew = a - 1;
+                    this._elements[a + 1] = obj;
+                }
+            }
+        }
+
+        delete this._elements[a];
+    }
 }
 
 module.exports = LinkedList;
--- test/linkedList.spec.js
+++ test/linkedList.spec.js
@@ -10,14 +10,14 @@ describe.only('#linkedList', function() {
         linkedList.push.should.be.instanceOf(Function);
     });
 
-    it('should #getLastElementNumber return NaN if there are no elements', function() {
+    it('should #getLastElementNumber returns NaN if there are no elements', function() {
         let linkedList = new algolib.LinkedList();
 
         let lastElementNum = linkedList.getLastElementNumber();
         lastElementNum.should.be.eql(NaN);
     });
 
-    it('should #getLastElementNumber return valid last element number (2 elements)', function() {
+    it('should #getLastElementNumber returns valid last element number (2 elements)', function() {
         let linkedList = new algolib.LinkedList();
 
         linkedList.push(5);
@@ -27,7 +27,7 @@ describe.only('#linkedList', function() {
         lastElementNum.should.be.eql(1);
     });
 
-    it('should #getLastElementNumber return valid last element number (6 elements)', function() {
+    it('should #getLastElementNumber returns valid last element number (6 elements)', function() {
         let linkedList = new algolib.LinkedList();
 
         linkedList.push(5);
@@ -41,7 +41,7 @@ describe.only('#linkedList', function() {
         lastElementNum.should.be.eql(5);
     });
 
-    it('should #get return valid value', function() {
+    it('should #get returns valid value', function() {
         let linkedList = new algolib.LinkedList();
 
         linkedList.push(5);
@@ -51,7 +51,7 @@ describe.only('#linkedList', function() {
         value.should.be.eql(3);
     });
 
-    it('should #getNext return valid link to the next element', function() {
+    it('should #getNext returns valid link to the next element', function() {
         let linkedList = new algolib.LinkedList();
 
         linkedList.push(5);
@@ -63,21 +63,21 @@ describe.only('#linkedList', function() {
         next.should.be.eql(3);
     });
 
-    it('should #del return NaN if argument < 0', function() {
+    it('should #del returns NaN if argument < 0', function() {
         let linkedList = new algolib.LinkedList();
 
         let del = linkedList.del(-4);
         del.should.be.eql(NaN);
     });
 
-    it('should #del return NaN if there are no existing elements', function() {
+    it('should #del returns NaN if there are no existing elements', function() {
         let linkedList = new algolib.LinkedList();
 
         let del = linkedList.del(0);
         del.should.be.eql(NaN);
     });
 
-    it('should #del return NaN if argument > then num of existing elements', function() {
+    it('should #del returns NaN if argument > then num of existing elements', function() {
         let linkedList = new algolib.LinkedList();
 
         linkedList.push(23);
@@ -96,7 +96,7 @@ describe.only('#linkedList', function() {
         lastElement.should.be.eql(NaN);
     });
 
-    it('should #getNext return link to next element after deleted', function() {
+    it('should #getNext returns link to next element after deleted', function() {
         let linkedList = new algolib.LinkedList();
 
         linkedList.push(23);
@@ -108,7 +108,7 @@ describe.only('#linkedList', function() {
         next.should.be.eql(2);
     });
 
-    it('should #get return valid value after exchanging link to next element', function() {
+    it('should #get returns valid value after exchanging link to next element', function() {
         let linkedList = new algolib.LinkedList();
 
         linkedList.push(23);
@@ -120,7 +120,7 @@ describe.only('#linkedList', function() {
         value.should.be.eql(23);
     });
 
-    it('should #getNextLink return valid value after deleting 2 elements', function() {
+    it('should #getNextLink returns valid value after deleting 2 elements', function() {
         let linkedList = new algolib.LinkedList();
 
         linkedList.push(23);
@@ -130,12 +130,64 @@ describe.only('#linkedList', function() {
         linkedList.push(12);
         linkedList.push(13);
         linkedList.del(1);
-        linkedList.del(4);
+        linkedList.del(2);
 
-        let next1 = linkedList.getNextLink(0);
-        let next2 = linkedList.getNextLink(3)
-        next1.should.be.eql(2);
-        next2.should.be.eql(5);
+        let next = linkedList.getNextLink(0);
+        next.should.be.eql(3);
+    });
+
+    it('should #getPrewLink returns valid value after deleting element', function() {
+        let linkedList = new algolib.LinkedList();
+
+        linkedList.push(23);
+        linkedList.push(3);
+        linkedList.push(15);
+        linkedList.del(1);
+
+        let prew = linkedList.getPrewLink(2);
+        prew.should.be.eql(0);
+    });
+
+    it('should #getLastElementNumber returns valid value after deleting element', function() {
+        let linkedList = new algolib.LinkedList();
+
+        linkedList.push(23);
+        linkedList.push(3);
+        linkedList.push(15);
+        linkedList.push(4);
+        linkedList.push(5);
+        linkedList.push(2);
+        linkedList.push(88);
+        linkedList.del(4);
+        linkedList.del(5);
+        linkedList.del(6);
+
+        let last = linkedList.getLastElementNumber();
+        last.should.be.eql(3);
+    });
+
+    it('should element has prewLink after deleting previously element', function() {
+        let linkedList = new algolib.LinkedList();
+
+        linkedList.push(23);
+        linkedList.push(3);
+        linkedList.push(15);
+        linkedList.push(4);
+        linkedList.push(5);
+        linkedList.push(2);
+        linkedList.push(88);
+        linkedList.del(3);
+        linkedList.del(5);
+        linkedList.push(88);
+        linkedList.push(88);
+        linkedList.del(7);
+
+        let prew1 = linkedList.getPrewLink(8);
+        let prew2 = linkedList.getPrewLink(6);
+        let prew3 = linkedList.getPrewLink(4);
+        prew1.should.be.eql(6);
+        prew2.should.be.eql(4);
+        prew3.should.be.eql(2);
     });
 
 })
