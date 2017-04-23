be22d489d03e9a91681d0596bb433ba7aa170d0a
Shchepkin Alex <shchepkin.alex@gmail.com>
2017-04-23 18:43:19 +0300

    refactoring project structure

---
+++ methods/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let bubbleSort = require('./bubble-sort/bubble-sort');
+let coverByTile = require('./coverByTile/coverByTile');
+let UnionFind = require('./unionFind/unionFind');
+let bitPlusPlus = require('./bitPlusPlus/bitPlusPlus');
+let LinkedList = require('./linkedList/linkedList');
+
+let algolib = {
+    bubbleSort,
+    coverByTile,
+    UnionFind,
+    bitPlusPlus,
+    LinkedList
+};
+
+module.exports = algolib;
