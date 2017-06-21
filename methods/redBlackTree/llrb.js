cebe6416d26e21464b044c50a4ddfbba98e83133
Shchepkin Alex <shchepkin.alex@gmail.com>
2017-06-22 01:23:22 +0300

    implement tests and methods size, getMax, getMin, find

--- methods/redBlackTree/llrb.js
+++ methods/redBlackTree/llrb.js
@@ -6,7 +6,7 @@ class Node{
         this.key = key;
         this.value = value;
         this.left = null;
-        this.righ = null;
+        this.right = null;
         this.isRed = true;
     }
 }
@@ -15,6 +15,7 @@ class Llrb {
 
     constructor(elements){
         this.root = null;
+        this.numOfNodes = 0;
 
         if (elements && elements.length){
            this._buildFromArray(elements);
@@ -23,18 +24,78 @@ class Llrb {
 
     _buildFromArray(){}
 
-    _findPlace(key){}
+    _rotateLeft(node){}
+    _rotateRight(node){}
+    _flipColor(node){}
+
+    _findPlace(node, key) {
+        if (node.key > key) {
+            if (!node.left) {return node;}
+            return this._findPlace(node.left, key);
+
+        } else if (node.key < key) {
+            if(!node.right){return node;}
+            return this._findPlace(node.right, key);
+        }
+    }
 
     insert(key, value){
         if(!this.root){
             return this.root = new Node (key, value);
         }
+
+        const parent = this._findPlace(this.root, key);
+
+        if (parent.key < key){
+            parent.right = new Node(key, value);
+
+        }else if (parent.key > key) {
+            parent.left = new Node(key, value);
+
+        }else {
+            parent.value = value;
+        }
+
+        //node.right = red --> _leftRotate
+        //left & right = red --> _flipColor
+        //node.left = red, node.left.left = _rightRotate
+        this.numOfNodes++;
     }
 
-    find(key){}
-    getMin(){}
-    getMax(){}
-    size(){}
+    find(key){
+        let _currentRoot = this.root;
+        while (_currentRoot) {
+            if (key > _currentRoot.key) {
+                _currentRoot = _currentRoot.right;
+            } else if (key < _currentRoot.key){
+                _currentRoot = _currentRoot.left;
+            } else return _currentRoot.value;
+        }
+    }
+
+    getMin() {
+        let _currentRoot = this.root;
+        while (_currentRoot) {
+            if (_currentRoot.left) {
+                _currentRoot = _currentRoot.left;
+            } else return _currentRoot;
+        }
+    }
+
+    getMax(){
+        let _currentRoot = this.root;
+        while (_currentRoot) {
+            if (_currentRoot.right) {
+                _currentRoot = _currentRoot.right;
+            } else return _currentRoot;
+        }
+    }
+
+    size(){
+        if(this.numOfNodes){
+            return this.numOfNodes + 1;
+        }else return this.numOfNodes;
+    }
 }
 
 module.exports = Llrb;
--- methods/redBlackTree/llrb.spec.js
+++ methods/redBlackTree/llrb.spec.js
@@ -4,12 +4,12 @@
 'use strict';
 
 // llrb - left lint red black tree
-const should = require();
+const should = require('should');
 const algolib = require('../index.js');
 
 describe.only('#Llrb', function() {
 
-    it('should Llrb create instance of tree', function() {
+    it('should #Llrb create instance of tree', function() {
         const  tree = new algolib.Llrb();
 
         tree.insert.should.be.instanceOf(Function);
@@ -19,23 +19,120 @@ describe.only('#Llrb', function() {
         tree.size.should.be.instanceOf(Function);
     });
 
-    it('should Llrb insert element', function() {
+    it('should #Llrb insert element in root', function() {
         const  tree = new algolib.Llrb();
 
         tree.insert(4, 'algorithms');
         should(tree.root).be.ok() ;
     });
 
-    it('should Llrb insert element', function() {
+    it('should #Llrb insert element in the right place', function() {
         const  tree = new algolib.Llrb();
 
         tree.insert(4, 'algorithms');
-        tree.insert(8, 'value new');
+        tree.insert(8, 'value right');
 
-        tree.right.key.should.be.eql(8);
-        tree.right.value.should.be.eql('value new');
+        tree.root.right.key.should.be.eql(8);
+        tree.root.right.value.should.be.eql('value new');
 
         should(tree.root).be.ok();
     });
 
-})
+    it('should #Llrb insert element in the left place', function() {
+        const  tree = new algolib.Llrb();
+
+        tree.insert(8, 'algorithms');
+        tree.insert(4, 'value left');
+
+        tree.root.left.key.should.be.eql(4);
+        tree.root.left.value.should.be.eql('value left');
+        should(tree.root).be.ok();
+    });
+
+    it('should #Llrb insert element in the left and the right places', function() {
+        const  tree = new algolib.Llrb();
+
+        tree.insert(8, 'algorithms');
+        tree.insert(4, 'value left');
+        tree.insert(10, 'value right');
+
+        tree.root.left.key.should.be.eql(4);
+        tree.root.left.value.should.be.eql('value left');
+        tree.root.right.key.should.be.eql(10);
+        tree.root.right.value.should.be.eql('value right');
+        should(tree.root).be.ok();
+    });
+
+    it('should #getMin return min element', function() {
+        const  tree = new algolib.Llrb();
+
+        tree.insert(20, 'value 20');
+        tree.insert(15, 'value 15');
+        tree.insert(30, 'value 30');
+        tree.insert(25, 'value 25');
+        tree.insert(27, 'value 27');
+        tree.insert(23, 'value 23');
+        tree.insert(17, 'value 17');
+        tree.insert(10, 'value 10');
+        tree.insert(6, 'value 6');
+
+        tree.getMin().key.should.be.eql(6);
+        tree.getMin().value.should.be.eql('value 6');
+        should(tree.root).be.ok();
+    });
+
+    it('should #getMax return max element', function() {
+        const  tree = new algolib.Llrb();
+
+        tree.insert(20, 'value 20');
+        tree.insert(15, 'value 15');
+        tree.insert(30, 'value 30');
+        tree.insert(25, 'value 25');
+        tree.insert(27, 'value 27');
+        tree.insert(23, 'value 23');
+        tree.insert(17, 'value 17');
+        tree.insert(10, 'value 10');
+        tree.insert(6, 'value 6');
+
+        tree.getMax().key.should.be.eql(30);
+        tree.getMax().value.should.be.eql('value 30');
+
+        tree.size().should.be.eql(9);
+        should(tree.root).be.ok();
+    });
+
+    it('should #size return 3 if inserted 3 elements', function() {
+        const  tree = new algolib.Llrb();
+
+        tree.insert(20, 'value 20');
+        tree.insert(15, 'value 15');
+        tree.insert(30, 'value 30');
+
+        tree.size().should.be.eql(3);
+        should(tree.root).be.ok();
+    });
+
+    it('should #size return 0 if there are no one element', function() {
+        const  tree = new algolib.Llrb();
+        tree.size().should.be.eql(0);
+    });
+
+    it.only('should #find return max element', function() {
+        const  tree = new algolib.Llrb();
+
+        tree.insert(20, 'value 20');
+        tree.insert(15, 'value 15');
+        tree.insert(30, 'value 30');
+        tree.insert(25, 'value 25');
+        tree.insert(27, 'value 27');
+        tree.insert(23, 'value 23');
+        tree.insert(17, 'value 17');
+        tree.insert(10, 'value 10');
+        tree.insert(6, 'value 6');
+
+        tree.find(27).should.be.eql('value 27');
+        should(tree.root).be.ok();
+    });
+
+
+});
